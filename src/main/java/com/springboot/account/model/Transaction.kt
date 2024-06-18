21073db535a0d8f608bba0db15fa6da3a3c24f26
Mecasu <melihcansunay@live.com>
2024-06-18 11:35:16 +0300

    dependencies added, account, customer, transaction created.

---
+++ src/main/java/com/springboot/account/model/Transaction.kt
@@ -0,0 +1,29 @@
+package com.springboot.account.model
+
+import jakarta.persistence.*
+import org.hibernate.Transaction
+import org.hibernate.annotations.GenericGenerator
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Entity
+data class Transaction(
+    @Id
+    @GeneratedValue(generator ="UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    val id: String?,
+    val transaction: TransactionType? = TransactionType.INITIAL,
+    val amount: BigDecimal?,
+    val transactionDate: LocalDateTime,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "account_id", nullable = false)
+    val account: Account?
+
+
+
+)
+
+enum class TransactionType {
+    INITIAL, TRANSFER
+}
